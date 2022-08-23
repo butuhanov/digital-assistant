9785a66653e63c3328a7820d0973a992da590cf7
Бутуханов Василий Анатольевич <butukhanovva@kadastr.ru>
2022-08-23 12:04:14 +0700

    #1 Отключение промежуточного слоя (instrumentingMiddleware)

--- services/stringsvc/main.go
+++ services/stringsvc/main.go
@@ -4,41 +4,41 @@ import (
 	"net/http"
 	"os"
 
-	stdprometheus "github.com/prometheus/client_golang/prometheus"
-	"github.com/prometheus/client_golang/prometheus/promhttp"
+	//stdprometheus "github.com/prometheus/client_golang/prometheus"
+	//"github.com/prometheus/client_golang/prometheus/promhttp"
 
 	"github.com/go-kit/kit/log"
-	kitprometheus "github.com/go-kit/kit/metrics/prometheus"
+	//kitprometheus "github.com/go-kit/kit/metrics/prometheus"
 	httptransport "github.com/go-kit/kit/transport/http"
 )
 
 func main() {
 	logger := log.NewLogfmtLogger(os.Stderr)
 
-	fieldKeys := []string{"method", "error"}
-	requestCount := kitprometheus.NewCounterFrom(stdprometheus.CounterOpts{
-		Namespace: "my_group",
-		Subsystem: "string_service",
-		Name:      "request_count",
-		Help:      "Number of requests received.",
-	}, fieldKeys)
-	requestLatency := kitprometheus.NewSummaryFrom(stdprometheus.SummaryOpts{
-		Namespace: "my_group",
-		Subsystem: "string_service",
-		Name:      "request_latency_microseconds",
-		Help:      "Total duration of requests in microseconds.",
-	}, fieldKeys)
-	countResult := kitprometheus.NewSummaryFrom(stdprometheus.SummaryOpts{
-		Namespace: "my_group",
-		Subsystem: "string_service",
-		Name:      "count_result",
-		Help:      "The result of each count method.",
-	}, []string{}) // no fields here
+	//fieldKeys := []string{"method", "error"}
+	//requestCount := kitprometheus.NewCounterFrom(stdprometheus.CounterOpts{
+	//	Namespace: "my_group",
+	//	Subsystem: "string_service",
+	//	Name:      "request_count",
+	//	Help:      "Number of requests received.",
+	//}, fieldKeys)
+	//requestLatency := kitprometheus.NewSummaryFrom(stdprometheus.SummaryOpts{
+	//	Namespace: "my_group",
+	//	Subsystem: "string_service",
+	//	Name:      "request_latency_microseconds",
+	//	Help:      "Total duration of requests in microseconds.",
+	//}, fieldKeys)
+	//countResult := kitprometheus.NewSummaryFrom(stdprometheus.SummaryOpts{
+	//	Namespace: "my_group",
+	//	Subsystem: "string_service",
+	//	Name:      "count_result",
+	//	Help:      "The result of each count method.",
+	//}, []string{}) // no fields here
 
 	var svc StringService
 	svc = stringService{}
 	svc = loggingMiddleware{logger, svc}
-	svc = instrumentingMiddleware{requestCount, requestLatency, countResult, svc}
+	//svc = instrumentingMiddleware{requestCount, requestLatency, countResult, svc}
 
 	uppercaseHandler := httptransport.NewServer(
 		makeUppercaseEndpoint(svc),
@@ -54,7 +54,7 @@ func main() {
 
 	http.Handle("/uppercase", uppercaseHandler)
 	http.Handle("/count", countHandler)
-	http.Handle("/metrics", promhttp.Handler())
+	//http.Handle("/metrics", promhttp.Handler())
 	logger.Log("msg", "HTTP", "addr", os.Getenv("PORT"))
 	logger.Log("err", http.ListenAndServe(os.Getenv("PORT"), nil))
 }
